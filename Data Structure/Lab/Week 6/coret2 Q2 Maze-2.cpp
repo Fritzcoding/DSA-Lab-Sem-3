d8228810770a92831070c58655affcb1fe03de80
Fritzcoding <fritzhans1689@gmail.com>
2025-10-30 02:10:28 +0800

    maze

--- Data Structure/Lab/Week 6/coret2 Q2 Maze-2.cpp
+++ Data Structure/Lab/Week 6/coret2 Q2 Maze-2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#define maxsize 10
+#define size 10
 #define start 8
 #define end 1
 using namespace std;
@@ -17,116 +16,110 @@ struct Element{
     int dir;
 };
 
-class MazeSolver{
+class Mazesolver{
 private:
-    int top = -1;
-    Offsets move[4]{ // right up left down
-        {0,1},{-1,0},{0,-1},{1,0} 
+    Offsets Move[4]{ //right up left down
+        {0,1}, {-1,0}, {0,-1}, {1,0}
     };
-    int maze[maxsize][maxsize];
-    char mark[maxsize][maxsize];
-    vector<Element> stack;
+    int maze[size][size];
+    char mark[size][size];
+    vector<Element> stackk;
 public:
-    MazeSolver(){
-        top = 0;
-        for(int i = 0; i < maxsize; i++){
-            for(int j = 0; j < maxsize; j++){
+    Mazesolver(){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                maze[i][j] = 0;
                 mark[i][j] = '0';
             }
         }
     }
-    void readmaze(){
-        for(int i = 0; i < maxsize; i++){
-            for(int j = 0; j < maxsize; j++){
+
+    void readpath(){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
                 cin >> maze[i][j];
             }
         }
     }
-    
+
     bool findpath(){
+        int row, col, dir, nextrow, nextcol;
+        Element position;
         bool found = false;
         mark[start][start] = 'S';
         mark[end][end] = 'X';
-        
-        int row, col, nextrow, nextcol, dir;
-        Element position;
         position.row = start;
         position.col = start;
         position.dir = 0;
-        stack.push_back(position);
-        
-        while(!stack.empty() && !found){
-            position = stack.back();
-            stack.pop_back();
+        stackk.push_back(position);
+
+        while(!found && !stackk.empty()){
+            position = stackk.back();
+            stackk.pop_back();
             row = position.row;
             col = position.col;
             dir = position.dir;
-            while(dir < 4){
-                nextrow = row + move[dir].ver;
-                nextcol = col + move[dir].hor;
-                
-                if(maze[nextrow][nextcol] == 0 && mark[nextrow][nextcol] == 'X'){
+            while(dir < 4 && !found){
+                nextrow = row + Move[dir].ver;
+                nextcol = col + Move[dir].hor;
+
+                if(mark[nextrow][nextcol] == 'X'){
                     found = true;
                 }
                 
                 if(found) break;
                 
-                if(maze[nextrow][nextcol] == 0 && mark[nextrow][nextcol] == '0'){
+                if(maze[nextrow][nextcol] == 0 && mark[nextrow][nextcol] == '0' && !found){
                     mark[nextrow][nextcol] = 'G';
                     position.row = row;
                     position.col = col;
                     position.dir = ++dir;
-                    stack.push_back(position);
+                    stackk.push_back(position);
+                    
                     row = nextrow;
                     col = nextcol;
                     dir = 0;
                 } else dir++;
-                
             }
-            if(dir >= 4 && (row != start && col != 8)){
+
+            if(dir >= 4 && mark[row][col] != 'S'){
                 mark[row][col] = 'D';
             }
         }
-        
         return found;
     }
-    
-    void printmaze(){
-        for(int i = 0; i < maxsize; i++){
-            for(int j = 0; j < maxsize; j++){
-                if (maze[i][j] == 1) {
-                    cout << "1";
+
+    void printpath(){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(maze[i][j] == 1){
+                    cout << '1';
                 }
-                else if (mark[i][j] == 'S') {
-                    cout << "S";
+                else if(mark[i][j] == 'G'){
+                    cout << 'G';
                 }
-                else if (mark[i][j] == 'X') {
-                    cout << "X";
+                else if(mark[i][j] == 'D'){
+                    cout << 'D';
                 }
-                else if (mark[i][j] == 'G') {
-                    cout << "G";
+                else if(mark[i][j] == 'X'){
+                    cout << 'X';
                 }
-                else if (mark[i][j] == 'D') {
-                    cout << "D";
+                else if(mark[i][j] == 'S'){
+                    cout << 'S';
                 }
-                else {
-                    cout << "0";
-                }
-                if (j < maxsize - 1) cout << " ";
+                else cout << '0';
             }
             cout << endl;
         }
     }
 };
 
-int main()
-{
-    MazeSolver solver;
-    solver.readmaze();
+int main(){
+    Mazesolver solver;
+    solver.readpath();
     if(solver.findpath()){
         cout << "YES" << endl;
     } else cout << "NO" << endl;
-    
-    solver.printmaze();
-    
+
+    solver.printpath();
 }
